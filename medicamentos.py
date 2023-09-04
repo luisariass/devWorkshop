0647d63af9fb7619dbfbac3ff9d5073192600243
luis ariasl <ariasl@utb.edu.com>
2023-09-04 10:14:50 -0500

    modificacion medicamentos

--- medicamentos.py
+++ medicamentos.py
@@ -1,36 +1,38 @@
-listaSignos = []
-class medicamentos():
-    def __init__(self, presure, temperature, hipo, frecuency) -> None:
-        self. presure = presure
-        self.temperature = temperature
-        self.hipo = hipo
-        self.frecuency = frecuency
+listSigns = []
 
-    def entregarSignos(self):
+
+class medicament:
+    def __init__(self, pressure, temperature, hippo, frequency) -> None:
+        self.pressure = pressure
+        self.temperature = temperature
+        self.hippo = hippo
+        self.frequency = frequency
+
+    def deliverSigns(self):
         print(
             "-" * 20,
-            "\nPresion: {}"
-            "\nTemperatura: {} "
-            "\nSaturacion: {}"
-            "\nFrecuencia: {}".format(self.presure, self.temperature, self.hipo, self.frecuency))
-             
-def validarSignos(presure,temperature,hipo,frecuency ):
-    
-    mensaje1 = "presion normal"
-    mensaje1 = "Tiene indicios de hipertension " if presure > 140 else mensaje1
-    mensaje1 = "Tiene indicios de hipotension " if presure < 90 else mensaje1
+            "\nPressure: {}"
+            "\nTemperature: {} "
+            "\nSaturation: {}"
+            "\nFrequency: {}".format(self.pressure, self.temperature, self.hippo, self.frequency))
 
-    mensaje2 = "Temperatura normal"
-    mensaje2 = "Tiene indicios de hipotermia " if temperature < 35 else mensaje2
-    mensaje2 = "Tiene indicios de hipertermia " if temperature > 38 else mensaje2 
 
-    mensaje3 = "Saturacion oxigeno normal"
-    mensaje3 = "Tiene indicios de hipoxemia " if hipo < 90 else mensaje3
-    mensaje3 = "Tiene indicios de hiperoxia " if hipo > 100 else mensaje3
+def validateSigns(pressure, temperature, hippo, frequency):
+    message1 = "Pressure normal"
+    message1 = "Has signs of hypertension " if pressure > 140 else message1
+    message1 = "Has signs of hypotension " if pressure < 90 else message1
 
-    mensaje4 = "Frecuencia respiratoria normal"
-    mensaje4 = "Tiene indicios de bradipnea " if frecuency < 12 else mensaje4
-    mensaje4 = "Tiene indicios de taquipnea " if frecuency > 20 else mensaje4
-                             #presion  #temperatura #saturacion #frecuencia
-    paciente2 = medicamentos(mensaje1, mensaje2, mensaje3, mensaje4) 
-    listaSignos.append(paciente2)
+    message2 = "Temperature normal"
+    message2 = "Has signs of hypothermia " if temperature < 35 else message2
+    message2 = "Has signs of hyperthermia " if temperature > 38 else message2
+
+    message3 = "normal oxygen saturation "
+    message3 = "Has signs of hypoxemia " if hippo < 90 else message3
+    message3 = "Has signs of hyperoxia " if hippo > 100 else message3
+
+    message4 = "Frecuencia respiratoria normal"
+    message4 = "Has signs of bradypnea " if frequency < 12 else message4
+    message4 = "Has signs of tachypnea " if frequency > 20 else message4
+    # presion  #temperatura #saturacion #frecuencia
+    patient2 = medicament(message1, message2, message3, message4)
+    listSigns.append(patient2)
